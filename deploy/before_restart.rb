1ddd0b978a611407e4d3454de71ed3b8ac118a0e
Greg Schulte <rikkitikkitumbo@gmail.com>
2014-08-08 01:02:30 -0700

    7

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,1 +1,1 @@
-run "cd /srv/www/writersarah/current && /usr/local/bin/bundle exec rake assets:precompile"
+# run "cd /srv/www/writersarah/current && /usr/local/bin/bundle exec rake assets:precompile"
