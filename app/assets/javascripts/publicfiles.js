394199a829d1e98b777bc5a4f62d6dc0a3e5d3d0
Greg Schulte <rikkitikkitumbo@gmail.com>
2014-08-16 17:39:53 -0700

    49

--- app/assets/javascripts/publicfiles.js
+++ app/assets/javascripts/publicfiles.js
@@ -2,14 +2,3 @@
 //= require jquery.min
 //= require jquery_ujs.js
 //= require bootstrap.min.js
-//= require angular.min
-//= require angular-animate.min
-//= require angular-ui-router.min
-//= require lodash.min
-//= require restangular.min
-
-//= require angular/public-app.js
-
-//= require angular/http.js.coffee
-//= require angular/routes.js.coffee
-//= require angular/public_controller.js.coffee
--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Writersarah::Application.routes.draw do
   end
 
 
-  root to: 'novels#index'
+  root to: "articles#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
